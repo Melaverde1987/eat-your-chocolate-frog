6a91c77dbaaeb1503f044a1c3ccd7119c44d9620
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-06-04 19:28:04 +0300

    change name selector

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-modal-review-open]"),
+      closeModalBtn: document.querySelector("[data-modal-review-close]"),
+      modal: document.querySelector("[data-modal-review]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
