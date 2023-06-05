7313fdb06beb8fa97f50faa94a251d3f4c22d46e
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-06-05 14:56:57 +0300

    fin

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -26,3 +26,48 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+// js Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 18,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 18,
+    },
+
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
