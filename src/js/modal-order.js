baca8e876e7f172d0d597cc6f3ee2bb8686df2bb
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-06-07 10:20:06 +0200

    top sellers correction

--- src/js/modal-order.js
+++ src/js/modal-order.js
@@ -1,1 +1,14 @@
-console.log('Test');
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-order-open]'),
+    closeModalBtn: document.querySelector('[data-modal-order-close]'),
+    modal: document.querySelector('[data-modal-order]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
