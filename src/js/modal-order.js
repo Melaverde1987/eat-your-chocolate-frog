8f06ffccead774b042435311591ab6dfb0a4ec6e
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-06-07 11:58:59 +0200

    modal update

--- src/js/modal-order.js
+++ src/js/modal-order.js
@@ -1,10 +1,12 @@
 (() => {
   const refs = {
+    openModalBtn2: document.querySelector('[data-hero-open]'),
     openModalBtn: document.querySelector('[data-modal-order-open]'),
     closeModalBtn: document.querySelector('[data-modal-order-close]'),
     modal: document.querySelector('[data-modal-order]'),
   };
 
+  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
